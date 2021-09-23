f5c796ec0eecbb613dede6e8910d3744a392f120
rogerioSystemDeveloper <rogerio.sis.proj@gmail.com>
2021-09-23 09:43:29 -0300

    1 file modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { MeuComponent } from './meu.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
